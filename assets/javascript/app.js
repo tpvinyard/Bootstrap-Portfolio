8b5c59d6e7c7bebe2f32c638b47bb924211ff9a8
Tommy Vinyard <tpvinyard@gmail.com>
2019-10-08 17:57:23 -0500

    adding contact formatting and center text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $('.scrollspy').scrollSpy();
 
   });
+
+$('#submit-contact').on('click', function() {
+  event.preventDefault();
+});
